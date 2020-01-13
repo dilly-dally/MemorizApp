c8b349e696f518d332ac17b652e240b3a209b0e7
Jiran Jeong <wlfks369@gmail.com>
2020-01-14 00:56:39 +0900

    setting android basic template

---
+++ app/src/main/java/com/dilly_dally/memorizapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dilly_dally.memorizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
